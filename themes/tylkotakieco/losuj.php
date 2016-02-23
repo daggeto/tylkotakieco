2dc20f1c1d8cdeba3dc8c0df72651443bb913991
daggeto <daggeto@gmail.com>
2016-02-23 20:54:41 +0000

    Fix Losoj page

--- themes/tylkotakieco/losuj.php
+++ themes/tylkotakieco/losuj.php
@@ -1,43 +1,51 @@
 <?php
 
-	$img = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `shity` WHERE `is_waiting`='0' ORDER BY RAND() LIMIT 1"));
-	$author = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `user` WHERE `id`='".$img['author']."'"));
-	list($records) = $img;
-	
-	if($records>=1) {
-		if($obj->isImage($img['type'])) {
-			if($conf->pobierz('img_title')) echo'<div class="img_title"><a href="obrazek.php?'.$img['id'].'">'.$img['title'].'</a></div>';
-			echo'<div class="shit">';
+$img = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `shity` WHERE `is_waiting`='0' ORDER BY RAND() LIMIT 1"));
+$author = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `user` WHERE `id`='" . $img['author'] . "'"));
+list($records) = $img;
+if ($records >= 1) {
+  if ($conf->pobierz('img_title')) echo '<div class="img_title"><a href="obrazek.php?' . $img['id'] . '">' . $img['title'] . '</a></div>';
+  echo '<div class="shit">';
 
-                        if($obj->isImage($img['type'])){
-                            echo  '<a href="obrazek.php?'.$img['id'].'"><img src="'.$img['img'].'" alt="'.$img['title'].'" /></a>';
-                        }
+  if ($obj->isImage($img['type'])) {
+    echo '<a href="obrazek.php?' . $img['id'] . '"><img src="' . $img['img'] . '" alt="' . $img['title'] . '" /></a>';
+  }
 
-                        if($obj->isFlash($img['type'])){
-                            $yt_width = 480;
-                            $yt_height = 390;
+  if ($obj->isFlash($img['type'])) {
+    $yt_width = 480;
+    $yt_height = 390;
 
-                            $id_filmu=str_replace("https://www.youtube.com/watch?v=", "", $img['img']);
+    $id_filmu = str_replace("https://www.youtube.com/watch?v=", "", $img['img']);
 
-                          echo '<iframe width="'.$yt_width.'" height="'.$yt_height.'"' .
-                            'src="http://www.youtube.com/embed/' . $id_filmu .'">' .
-                            '</iframe>';
-                        }
+    echo '<iframe width="' . $yt_width . '" height="' . $yt_height . '"' . 'src="http://www.youtube.com/embed/' . $id_filmu . '">' . '</iframe>';
+  }
 
-                        echo '<div id="info">'
-                                            .'<div class="social-info table-row to-left">'
-                                                .'<div class="fb-like" data-href="http://'.$conf->host().'/obrazek.php?'.$img['id'].'" data-send="true" data-layout="box_count" data-width="100%" data-show-faces="false" data-colorscheme="light" data-font="verdana"></div>'
-                                            .'</div>'
-                                            .'<div class="table-row">'
-                                                .'<div class="to-left">' . $content->getValue("global","dodano") . ': ' . $obj->getDateFromDateTime($img['data']) . '</div>'
-                                                .'<div class="to-right">' . $content->getValue("obrazek","dodanoPrzez") . ': ' . $author['login'] . '</div>'
-                                            .'</div>'
-                                        .'</div>'
-                        .'</div>';
-		}
-	}
-	else {
-		echo '<div class="shit">' . $content->getValue("global","brakWiadomosci") . '</div>';
-	}
-		
+  echo '<div id="info">' .
+        '<div class="social-info table-row to-left">' .
+          '<div
+              class="fb-like"
+              data-href="http://' . $conf->host() . '/obrazek.php?' . $img['id'] . '"
+              data-send="true"
+              data-layout="box_count"
+              data-width="100%"
+              data-show-faces="false"
+              data-colorscheme="light"
+              data-font="verdana">' .
+          '</div>' .
+        '</div>' .
+      '<div class="table-row">' .
+
+      '<div class="to-left">' .
+        $content->getValue("global", "dodano") . ': ' . $obj->getDateFromDateTime($img['data']) .
+      '</div>' .
+
+      '<div class="to-right">' . $content->getValue("obrazek", "dodanoPrzez") . ': ' . $author['login'] . '</div>' .
+    '</div>' .
+  '</div>' .
+'</div>';
+}
+else {
+  echo '<div class="shit">' . $content->getValue("global", "brakWiadomosci") . '</div>';
+}
+
 ?>
--- themes/tylkotakieco/poczekalnia.php
+++ themes/tylkotakieco/poczekalnia.php
@@ -1,54 +1,43 @@
 <?php
-	$obj->tabela("shity", "1");
-	$obj->getName('page');
-	$obj->ileNaStrone($conf->pobierz('img_na_strone'));
-	$query = $obj->pobierz();
-	
+$obj->tabela("shity", "1");
+$obj->getName('page');
+$obj->ileNaStrone($conf->pobierz('img_na_strone'));
+$query = $obj->pobierz();
 
-	if(mysqli_num_rows($query)) {
-		while($img = mysqli_fetch_array($query)) {
-                    $author = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `user` WHERE `id`='".$img['author']."'"));
-                    if($conf->pobierz('img_title')) echo'<div class="img_title"><a href="obrazek.php?'.$img['id'].'">'.$img['title'].'</a></div>';
-                    echo'<div class="shit">';
 
-                    if($obj->isImage($img['type'])){
-                        echo '<img src="'.$img['img'].'" alt="'.$img['title'].'" />';
-                    }
+if (mysqli_num_rows($query)) {
+  while ($img = mysqli_fetch_array($query)) {
+    $author = mysqli_fetch_array(mysqli_query($db, "SELECT * FROM `user` WHERE `id`='" . $img['author'] . "'"));
+    if ($conf->pobierz('img_title')) echo '<div class="img_title"><a href="obrazek.php?' . $img['id'] . '">' . $img['title'] . '</a></div>';
+    echo '<div class="shit">';
 
-                    if($obj->isFlash($img['type'])){
-                        $yt_width = 480;
-                        $yt_height = 390;
+    if ($obj->isImage($img['type'])) {
+      echo '<img src="' . $img['img'] . '" alt="' . $img['title'] . '" />';
+    }
 
-                        $id_filmu=str_replace("https://www.youtube.com/watch?v=", "", $img['img']);
+    if ($obj->isFlash($img['type'])) {
+      $yt_width = 480;
+      $yt_height = 390;
 
-                      echo '<iframe width="'.$yt_width.'" height="'.$yt_height.'"' .
-                        'src="http://www.youtube.com/embed/' . $id_filmu .'">' .
-                        '</iframe>';
-                    }
+      $id_filmu = str_replace("https://www.youtube.com/watch?v=", "", $img['img']);
 
-                    echo '<div id="info">'
-                            .'<div class="social-info table-row to-left">'
-                                .'<div class="fb-like" data-href="http://'.$conf->host().'/obrazek.php?'.$img['id'].'" data-send="true" data-layout="box_count" data-width="100%" data-show-faces="false" data-colorscheme="light" data-font="verdana"></div>'
-                            .'</div>'
-                            .'<div class="table-row">'
-                                .'<div class="to-left">' . $content->getValue("global","dodano") . ': ' . $obj->getDateFromDateTime($img['data']) . '</div>'
-                                .'<div class="to-right">' . $content->getValue("obrazek","dodanoPrzez") . ': ' . $author['login'] . '</div>'
-                            .'</div>'
-                        .'</div>'
-                    .'</div>';
-                    }
-            if($conf->pobierz('reklama')) adModule($db);
-	}
-	else {
-		echo '<div style="text-align:center;">' . $content->getValue("global","brakWiadomosci") . '</div>';
-	}
+      echo '<iframe width="' . $yt_width . '" height="' . $yt_height . '"' . 'src="http://www.youtube.com/embed/' . $id_filmu . '">' . '</iframe>';
+    }
+
+    echo '<div id="info">' . '<div class="social-info table-row to-left">' . '<div class="fb-like" data-href="http://' . $conf->host() . '/obrazek.php?' . $img['id'] . '" data-send="true" data-layout="box_count" data-width="100%" data-show-faces="false" data-colorscheme="light" data-font="verdana"></div>' . '</div>' . '<div class="table-row">' . '<div class="to-left">' . $content->getValue("global", "dodano") . ': ' . $obj->getDateFromDateTime($img['data']) . '</div>' . '<div class="to-right">' . $content->getValue("obrazek", "dodanoPrzez") . ': ' . $author['login'] . '</div>' . '</div>' . '</div>' . '</div>';
+  }
+  if ($conf->pobierz('reklama')) adModule($db);
+}
+else {
+  echo '<div style="text-align:center;">' . $content->getValue("global", "brakWiadomosci") . '</div>';
+}
 ?>
 <div class="strony">
-<?php
-	if($obj->getName() != NULL)  $page = $obj->getName();
-	else $page = 1;
-	if($obj->prevStrona()) echo '<a href="?page='.($page-1).'">&lt; ' . $content->getValue("global","poprzedni") . '</a> ';
-	$obj->strony();
-	if($obj->nextStrona()) echo ' <a href="?page='.($page+1).'">' . $content->getValue("global","nastepny") . ' &gt;</a>';
-?>
+  <?php
+  if ($obj->getName() != NULL) $page = $obj->getName();
+  else $page = 1;
+  if ($obj->prevStrona()) echo '<a href="?page=' . ($page - 1) . '">&lt; ' . $content->getValue("global", "poprzedni") . '</a> ';
+  $obj->strony();
+  if ($obj->nextStrona()) echo ' <a href="?page=' . ($page + 1) . '">' . $content->getValue("global", "nastepny") . ' &gt;</a>';
+  ?>
 </div>
